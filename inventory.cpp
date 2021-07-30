e66e3856cdffc5dbea13f1868f4979aee7c6238c
Michelle Nguyen <michellenguyen11239@aol.com>
2021-07-29 18:29:00 -0700

    Updated inventory.cpp with function prototype to look for product code in db

--- inventory.cpp
+++ inventory.cpp
@@ -4,3 +4,25 @@
 #include <string>
 #include "inventory.hpp" // Include function prototype folder
 using namespace std;
+
+// This function will create a file stream object, read in the file, and return the product and price
+string locateProduct(int number) {
+
+    ifstream inputFile; // Create file stream object
+    inputFile.open("./db/inventory.dat"); // Open the file
+    string line; // String variable that holds the product in the file
+    string productName2;
+
+    while (inputFile >> line) // Reads in the lines
+    {
+        if (to_string(number) == line)
+        {
+            inputFile.ignore();
+            getline(inputFile, productName2);
+            return productName2;
+        }
+
+    }
+    inputFile.close(); // Close the file
+    return "Not found";
+}
